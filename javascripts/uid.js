cee4a035395bf48a4cce8a25a768fb476792282e
Amy Lee <amyleeannebennett@gmail.com>
2015-11-17 09:41:39 -0600

    commented out changes working with authentication. switching branches

--- javascripts/uid.js
+++ javascripts/uid.js
@@ -6,6 +6,25 @@ define(function(require) {
     },
     setUid: function(newId) {
       uid = newId;
-    }
+    },
+    
+ //    getProfile: function() {
+	// return github.profileImageURL;
+	// },
+
+	// setProfile: function(newProfile) {
+	// 	github.profileImageUrl = newProfile;
+	// },
+
+	// getName: function() {
+	// 	return github.profileImageURL;
+	// },
+
+	// setName: function(newName) {
+	// 	github.displayName = newName;
+	// }
   };
 });
+
+
+
